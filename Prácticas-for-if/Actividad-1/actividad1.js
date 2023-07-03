42561ec3ea599ba4c2ce6c286dbc8ce028f3b5b5
LeoMontani <leonel.montani1@gmail.com>
2023-07-03 19:35:15 -0300

    Acá están las actividades resueltas de Javascript

---
+++ Prácticas-for-if/Actividad-1/actividad1.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+  if (i % 2 === 0) {
+    console.log("Es par");
+  }
+  else {
+    console.log("Es impar");
+  }
+}
---
+++ actividad2.js
@@ -0,0 +1,11 @@
+let numeroPrincipal = 7;
+if (numeroPrincipal <= 10) {
+  console.log("El número es válido");
+}
+else {
+  console.log("El número no es válido, debe ser un número entre el 1 y el 10");
+}
+for (let i = 0; i <= 10; i++) {
+  let tablaDelSiete = numeroPrincipal * i;
+  console.log(tablaDelSiete);
+}
